f767da63e66bce7086c4e2959156c92adbe02016
marcosdevelo <marcosgomezda@gmail.com>
2019-07-25 23:59:56 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Navbar } from "./navbar";
 //create your first component
 export class Home extends React.Component {
 	render() {
@@ -20,7 +20,28 @@ export class Home extends React.Component {
 					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
 					with love!
 				</p>
-			</div>
+			</div><li class="nav-item">
+							<a class="nav-link" href="#">
+								Features
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								Pricing
+							</a>
+						</li>
+						<li class="nav-item">
+							<a
+								class="nav-link disabled"
+								href="#"
+								tabindex="-1"
+								aria-disabled="true">
+								Disabled
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		);
 	}
 }
